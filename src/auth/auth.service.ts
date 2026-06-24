93225273f0739bb5e1295cd340586ef37f24e13e
Majd Assidi <assidimajd6@gmail.com>
2026-06-24 17:12:12 +0300

    Initial commit for bookstore-api-v2

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService{
+    
+    signin(){return'hello';}
+
+    signup(){return 'hi';}
+}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService extends PrismaClient{
+    
+    constructor(){
+
+
+
+        
+    }}
